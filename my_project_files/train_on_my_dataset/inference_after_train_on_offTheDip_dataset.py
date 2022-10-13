5a4ed3a2d6c6a5a129ab12f446ea94aabe274830
Harel <harel@lynxight.com>
2022-10-13 15:02:00 +0300

    try to push new vids

--- my_project_files/train_on_my_dataset/inference_after_train_on_offTheDip_dataset.py
+++ my_project_files/train_on_my_dataset/inference_after_train_on_offTheDip_dataset.py
@@ -25,23 +25,23 @@ result = inference_detector(model, img)
 model.show_result(img, result, out_file='my_project_files/data_for_inference/output/1211_res.jpg')
 
 # Let's try inference a video
-videos_to_infer = ["out_4_resized_video_full", "3_video_full", "not_in_dataset_vid2_resized_video_full"]
-for vid in videos_to_infer:
-    parse_video(
-        #video='my_project_files/data_for_inference/out_4_resized_video_from_after_labeling.mp4',
-        video=f'/home/ubuntu/Downloads/{vid}.mp4',
-        architecture_config='train_dump/my_config.py',
-        checkpoints='train_dump/latest.pth',
-        #result_path='my_project_files/data_for_inference/output/video_4_after_labeling_with_pocket_res.mp4',
-        result_path=f'/home/ubuntu/Downloads/output/out_{vid}_res.mp4',
-        #show=True,  # uncomment to show video inference live
-    )
-# parse_video(
-#         video='my_project_files/data_for_inference/out_4_resized_video_from_after_labeling.mp4',
-#         #video=f'my_project_files/data_for_inference/{vid}.mp4',
+# videos_to_infer = ["out_4_resized_video_full", "3_video_full", "not_in_dataset_vid2_resized_video_full"]
+# for vid in videos_to_infer:
+#     parse_video(
+#         #video='my_project_files/data_for_inference/out_4_resized_video_from_after_labeling.mp4',
+#         video=f'/home/ubuntu/Downloads/{vid}.mp4',
 #         architecture_config='train_dump/my_config.py',
 #         checkpoints='train_dump/latest.pth',
-#         result_path='my_project_files/data_for_inference/output/video_4_after_labeling_with_pocket_res.mp4',
-#         #result_path=f'my_project_files/data_for_inference/output/out_{vid}_res.mp4',
-#         show=True,  # uncomment to show video inference live
+#         #result_path='my_project_files/data_for_inference/output/video_4_after_labeling_with_pocket_res.mp4',
+#         result_path=f'/home/ubuntu/Downloads/output/out_{vid}_res.mp4',
+#         #show=True,  # uncomment to show video inference live
 #     )
+parse_video(
+        video='my_project_files/data_for_inference/more_videos_not_in_dataset_already_resized/more_vid_not_in_dataset_3.mp4',
+        #video=f'my_project_files/data_for_inference/{vid}.mp4',
+        architecture_config='train_dump/my_config.py',
+        checkpoints='train_dump/latest.pth',
+        result_path='my_project_files/data_for_inference/output/more_vid_not_in_dataset_3_res.mp4',
+        #result_path=f'my_project_files/data_for_inference/output/out_{vid}_res.mp4',
+        show=True,  # uncomment to show video inference live
+    )
